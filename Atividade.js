d0d17fe55d43d0f868fca3ff4829016535f861f4
Matheus-Ferreira-Lopes <matheuspalestra75@gmail.com>
2026-02-10 14:33:30 -0300

    Add files via upload

---
+++ Atividade.js
@@ -0,0 +1,69 @@
+let usuario = {
+  login: "Matheus",
+  senha: "12345",
+  tipo: "administrador"
+};
+console.log("O login do administrador é admin e a senha é 12345, muito fraca por sinal.");
+
+let aluno = {
+  nome: "Matheus",
+  curso: "Desenvolvimento de Sistemas",
+  turma: "DSI 3"
+};
+console.log("O aluno Matheus está matriculado no curso de Desenvolvimento de Sistemas, na turma DSI 3.");
+
+let professor = {
+  nome: "Matheus",
+  disciplina: "JavaScript",
+  turno: "manhã"
+};
+console.log("O professor Matheus comanda a disciplina de JavaScript no turno da manhã.");
+
+let empresa = {
+  nome: "SENAI",
+  area: "TI",
+  cidade: "Guarulhos"
+};
+console.log("A empresa SENAI atua na área de TI e fica em Guarulhos.");
+
+let computador = {
+  marca: "Asus,",
+  processador: " AMD Ryzen 5 5500 3.6GHz",
+  memoria: "16GB"
+};
+console.log("O computador da marca Asus possui processador AMD Ryzen 5 5500 3.6GHz e memória de 16GB.");
+
+let livro = {
+  titulo: "Supernatural: O Diário de John Winchester",
+  autor: " Alex Irvine",
+  paginas: 176
+};
+console.log("O livro Supernatural: O Diário de John Winchester  foi escrito por Alex Irvine e possui 176 páginas.");
+
+let filme = {
+  nome: "Supernatural",
+  genero: "Terror",
+  ano: 2005
+};
+console.log("O filme Supernatural é do gênero Terror e foi lançado em 2005.");
+
+let jogo = {
+  nome: "Fortnite",
+  plataforma: "PC",
+  genero: "Battle Royale"
+};
+console.log("O jogo Fortnite roda na plataforma PC e pertence ao gênero Battle Royale.");
+
+let conta = {
+  usuario: "Matheus",
+  senha: "1234",
+  nivel: "administrador"
+};
+console.log("O usuário Matheus possui nível administrador e utiliza a senha 1234, muito fraca por sinal.");
+
+let escola = {
+  nome: "SESI 427",
+  cidade: "Guarulhos",
+  turno: "manhã"
+};
+console.log("A escola SESI 427 funciona na cidade de Guarulhos no turno da manhã.");
